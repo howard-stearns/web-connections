53c3e4fc24df31b3fcc7392d90c61e7b88da0410
howard-stearns <howard@highfidelity.io>
2020-04-05 20:03:03 -0700

    face ai test

---
+++ public/face.js
@@ -0,0 +1,67 @@
+'use strict';
+console.log(faceapi.nets)
+
+async function faceTest() {
+    var start = Date.now();
+    await Promise.all([
+        faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('/models')
+    ]);
+    console.log('model', Date.now() - start);
+    const groupResult = await faceapi.detectAllFaces(group).withFaceLandmarks().withFaceDescriptors();
+    console.log('groupResult', groupResult, Date.now() - start);
+    const faceMatcher = new faceapi.FaceMatcher(groupResult)
+    console.log('matcher', faceMatcher);
+
+    const displaySize = { width: group.width, height: group.height };
+    faceapi.matchDimensions(groupCanvas, displaySize);
+    const resizedDetections = faceapi.resizeResults(groupResult, displaySize);
+    faceapi.draw.drawDetections(groupCanvas, resizedDetections)
+
+
+    const result = await faceapi.detectSingleFace(sheldonXpng).withFaceLandmarks().withFaceDescriptor();
+    console.log('result', result, Date.now() - start);
+    const bestMatch = faceMatcher.findBestMatch(result.descriptor);
+    console.log(bestMatch.toString());
+    groupResult.forEach(r => {
+        const dist = faceapi.euclideanDistance(r.descriptor, result.descriptor);
+        const box = r.detection.box;
+        const text = [dist.toPrecision(2)];
+        const anchor = box;
+        const drawOptions = {
+            anchorPosition: 'TOP_LEFT',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)'
+        };
+        const drawBox = new faceapi.draw.DrawTextField(text, anchor, drawOptions);
+        drawBox.draw(groupCanvas);
+    });
+}    
+//faceTest();
+
+async function webcamCapture(start) {
+    const groupResult = await faceapi.detectAllFaces(webcamVideo, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions();
+    console.log('groupResult', groupResult, Date.now() - start);
+    const displaySize = { width: webcamVideo.videoWidth, height: webcamVideo.videoHeight };
+    faceapi.matchDimensions(videoOverlay, displaySize);
+    const resizedDetections = faceapi.resizeResults(groupResult, displaySize);
+    faceapi.draw.drawDetections(videoOverlay, resizedDetections)
+    faceapi.draw.drawFaceExpressions(videoOverlay, resizedDetections, 0.05);
+    webcamCapture(Date.now());
+}
+async function webcamSetup(start) {
+    await Promise.all([
+        navigator.mediaDevices.getUserMedia({video: true})
+            .then(stream => new Promise(resolve => {
+                webcamVideo.srcObject = stream;
+                setTimeout(_ => resolve(stream), 1000);
+            })),
+        
+        //faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('/models')
+    ]);
+    console.log('model', Date.now() - start);
+    webcamCapture(Date.now());
+}
+webcamSetup();
